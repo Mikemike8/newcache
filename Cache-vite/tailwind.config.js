cf77926a66a9289c8df483eae90b4fe6ef651ae0
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-01-22 19:02:13 -0600

    vite

--- Cache-vite/tailwind.config.js
+++ Cache-vite/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the paths based on your project
   ],
   darkMode: "class", // Enable dark mode using class
   theme: {
